3d434fbd614361c0e2e2252eb9746673b921d8b4
Stanislas Bernard <bernardstanislas@gmail.com>
2018-12-19 16:18:13 +0100

    :sparkles: Read first article

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ArticleDto } from './article.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/articles')
+  getArticles(): ArticleDto {
+    return {
+      name: 'lol',
+      content: 'yolo',
+    };
   }
 }
---
+++ src/article.dto.ts
@@ -0,0 +1,4 @@
+export interface ArticleDto {
+  name: string;
+  content: string;
+}
