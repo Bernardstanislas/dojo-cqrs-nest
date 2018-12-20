93f6efb1d7e896fc1ac97ca5d6dacc389c742fe6
Stanislas Bernard <bernardstanislas@gmail.com>
2018-12-20 11:00:54 +0100

    :sparkles: Add command bus

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,16 +3,23 @@ import { ArticleDto } from './article.dto';
 import { Article } from './article.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateArticleCommand } from './commands/implementations/article-created.command';
 
 @Injectable()
 export class AppService {
-  constructor(@InjectRepository(Article) private readonly articleRepository: Repository<Article>) {}
+  constructor(
+    @InjectRepository(Article) private readonly articleRepository: Repository<Article>,
+    private readonly commandBus: CommandBus,
+  ) {}
 
   getHello(): string {
     return 'Hello World!';
   }
 
   async storeArticle(articleDto: ArticleDto): Promise<Article> {
+    const createArticleCommand = new CreateArticleCommand(articleDto);
+    await this.commandBus.execute(createArticleCommand);
     const article = this.articleRepository.create(articleDto);
     return this.articleRepository.save(article);
   }
