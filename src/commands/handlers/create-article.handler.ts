3f0702af244742e3cd44011a9327b82a5781fb06
Stanislas Bernard <bernardstanislas@gmail.com>
2018-12-20 18:16:39 +0100

    :sparkles: Handler now saves the article

--- src/commands/handlers/create-article.handler.ts
+++ src/commands/handlers/create-article.handler.ts
@@ -1,9 +1,15 @@
 import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
 import { CreateArticleCommand } from '../implementations/article-created.command';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Article } from '../../article.entity';
 
 @CommandHandler(CreateArticleCommand)
 export class CreateArticleHandler implements ICommandHandler<CreateArticleCommand> {
+  constructor(@InjectRepository(Article) private readonly articleRepository: Repository<Article>) {}
+
   execute(command: CreateArticleCommand, resolve: (value?) => void): any {
-    resolve();
+    const article = this.articleRepository.create(command.articleDto);
+    resolve(this.articleRepository.save(article));
   }
 }
